f6b16e04a75c8d00f69988bfc9a3f4f2883c9084
dalet1810 <50907189+dalet1810@users.noreply.github.com>
2020-01-20 12:21:27 +0200

    add storage names

--- duart.h
+++ duart.h
@@ -1,5 +1,7 @@
 // duart.h
-  
+
 void uart_task(void *v);
 esp_err_t get_saved_blob(char *, int);
 int getArgs(char *, char **, int);
+esp_err_t save_nm_blob(char *, char *);
+esp_err_t get_named_blob(char *, char *, int);
