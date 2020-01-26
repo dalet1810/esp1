f17a76fece45f6823c1e0183d31a14fce737a928
dalet1810 <50907189+dalet1810@users.noreply.github.com>
2020-01-26 12:06:35 +0200

    debugging blob and str, no results

--- duart.h
+++ duart.h
@@ -5,3 +5,6 @@ esp_err_t get_saved_blob(char *, int);
 int getArgs(char *, char **, int);
 esp_err_t save_nm_blob(char *, char *);
 esp_err_t get_named_blob(char *, char *, int);
+
+esp_err_t save_nm_str(char *, char *);
+esp_err_t get_named_str(char *, char *, int);
